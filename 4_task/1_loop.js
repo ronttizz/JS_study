903f0fa24ef7ce9a5d12d13f97d2911cd9550b8b
Nissinen Toni <s2200129@BCC02GRWQ6LRAir.lan>
2022-01-28 16:51:00 +0200

    Looo training to task #7, Task 7 not tested yet

--- 4_task/1_loop.js
+++ 4_task/1_loop.js
@@ -67,6 +67,17 @@ console.log("The average of the numbers were " + total / i);
 
 // 7. Make a program that ask first one number from the user. After that the program asks: ”Do you want to continue giving numbers?(y/n)”. If user answers y, the program continues to ask another number. If user answers n, program ends. In the end program prints out average of the numbers.
 
+let total = Number(prompt("Give a number"));
+let count = 0;
+let answer = "";
+do {
+  total += Number(prompt("Give a number"));
+  count++;
+} while (
+  (answer = prompt("Do you want to continue giving numbers?") === "n" || "N")
+);
+console.log("The average of the numbers were: " + total / count);
+
 // 8. Make a program that asks first how many numbers user wants to give to the program. After that program asks those numbers. In the end program prints out the smallest number that user gave.
 
 // 9. Make a program that asks ten numbers and in the end prints out two biggest numbers.

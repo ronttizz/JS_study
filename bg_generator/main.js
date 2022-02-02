f3d78086331e26c4233928a44f89eaa4415376a9
Nissinen Toni <s2200129@BC-C02GR0BWQ6LR-MacBook-Air.local>
2022-02-02 12:45:40 +0200

    Added more directions to gradient and commented out earlier version of the buttons

--- bg_generator/main.js
+++ bg_generator/main.js
@@ -3,7 +3,7 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let result = document.querySelector(".code");
 
-let directions = document.querySelectorAll('input[name="direction"]');
+let directions = document.querySelectorAll('input[name="directionButton"]');
 
 const setGradient = () => {
   let selectedDirection;
